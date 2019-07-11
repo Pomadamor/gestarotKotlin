87528ce65a2fd2fbebf58905608308eb203d5ec3
Ines ECK <ines@azconception.com>
2019-07-11 08:53:36 +0200

    first commit

---
+++ app/src/main/java/com/example/lerna/gestarot/activity/menu/LegalTermActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lerna.gestarot.activity.menu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LegalTermActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_legal_term)
+    }
+}
